15b8d4cfe54e055ac1435ea9bbda9e1c0a37daf1
Ram Murat Sharma <rammurat.rms.sharma0@gmail.com>
2022-07-11 16:47:07 +0100

    Build error fixed

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,9 @@ module.exports = {
     devServer: {
         static: './dist',
     },
+    stats: {
+        children: true
+    },
     devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
